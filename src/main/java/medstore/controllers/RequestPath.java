9b1c8bbdaf28e50711d1be43963122bfe09f0dc3
adiktz <gauravraviji@gmail.com>
2015-04-20 14:09:02 +0530

    initializing repo

---
+++ src/main/java/medstore/controllers/RequestPath.java
@@ -0,0 +1,18 @@
+package medstore.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestPath {
+
+	@RequestMapping("/")
+	  public String hello() {
+	    return "Hello World!";
+	  }
+	
+	@RequestMapping("/santa")
+	public String saySanta(){
+		return "This is santa Test";
+	}
+}
