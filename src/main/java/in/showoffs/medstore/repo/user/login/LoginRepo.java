8f009623ecd1ccc56159c946dedaac6c58148a48
adiktz <gauravraviji@gmail.com>
2015-04-26 16:56:01 +0530

    modifying loginRepo

--- src/main/java/in/showoffs/medstore/repo/user/login/LoginRepo.java
+++ src/main/java/in/showoffs/medstore/repo/user/login/LoginRepo.java
@@ -5,5 +5,5 @@ import in.showoffs.medstore.domain.user.login.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginRepo extends JpaRepository<Login, String>{
-
+ 
 }
